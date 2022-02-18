f5a82c9660965d8ea53e099fbe3de9581a80e93a
Nataliia Volynets <natasha99zt@gmail.com>
2022-02-18 20:03:09 +0200

    Add comments to `exchanger_helper.rb`

--- app/helpers/exchanger_helper.rb
+++ app/helpers/exchanger_helper.rb
@@ -1,8 +1,7 @@
 module ExchangerHelper
-
+  # Reads the country codes from a file to populate `to_currency` select field
   def countries_for_select
     file = File.read("#{Rails.root}/app/helpers/countries_codes_list.json")
     countries = JSON.parse(file)
   end
-
 end
